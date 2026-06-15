098755f8a368488542f70f89352d685c50364403
ducyb116vn <ducockutyb@gmail.com>
2026-06-15 18:37:47 +0700

    Delete common directory

--- common/Text.jsx
+++
@@ -1,7 +0,0 @@
-import { cn } from "../../../utils";
-
-const Text = ({ children, className = "" }) => {
-    return <p className={cn('text-white font-medium', className)}>{children}</p>;
-}
-
-export default Text
